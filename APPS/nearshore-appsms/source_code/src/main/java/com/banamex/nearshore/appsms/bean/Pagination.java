0011086a68a0f4e932015c9f4a1d2497519e676d
Guillermo <gcruz@anzen.com.mx>
2017-05-29 12:02:10 -0500

    add Pagination class

---
+++ APPS/nearshore-appsms/source_code/src/main/java/com/banamex/nearshore/appsms/bean/Pagination.java
@@ -0,0 +1,20 @@
+package com.banamex.nearshore.appsms.bean;
+
+public class Pagination {
+
+	private Integer index;
+	private Integer rows;
+	
+	public Integer getIndex() {
+		return index;
+	}
+	public void setIndex(Integer index) {
+		this.index = index;
+	}
+	public Integer getRows() {
+		return rows;
+	}
+	public void setRows(Integer rows) {
+		this.rows = rows;
+	}
+}
---
+++ CATALOGS/nearshore-catalogsms/source_code/src/main/java/com/banamex/nearshore/catalogsms/domain/Pagination.java
@@ -0,0 +1,23 @@
+package com.banamex.nearshore.catalogsms.domain;
+
+public class Pagination {
+	
+	private Integer index;
+	private Integer rows;
+	
+	public Integer getIndex() {
+		return index;
+	}
+	public void setIndex(Integer index) {
+		this.index = index;
+	}
+	public Integer getRows() {
+		return rows;
+	}
+	public void setRows(Integer rows) {
+		this.rows = rows;
+	}
+	
+	
+
+}
